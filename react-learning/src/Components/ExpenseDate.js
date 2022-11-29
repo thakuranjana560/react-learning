038e1b42e339203df1b3ed50cca883b1e8db2f65
Anjana Thakur <thakuranjana560@gmail.com>
2022-11-29 20:36:04 +0100

    Fixes Lint

--- react-learning/src/Components/ExpenseDate.js
+++ react-learning/src/Components/ExpenseDate.js
@@ -4,7 +4,6 @@ function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
-
   return (
     <div className="expense-date">
       <div>{month}</div>
